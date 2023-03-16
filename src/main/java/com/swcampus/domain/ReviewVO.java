9051d22e544070e1d1f34c1b6e0a1e15a59f9675
KOSTA <KOSTA@DESKTOP-EHLO97G>
2023-03-16 15:42:35 +0900

    2023-03-16 CKJ: 회원가입 구현
    - register 구현중

--- src/main/java/com/swcampus/domain/ReviewVO.java
+++ src/main/java/com/swcampus/domain/ReviewVO.java
@@ -1,5 +1,15 @@
 package com.swcampus.domain;
 
-public class Review {
+import lombok.Data;
 
+@Data
+public class ReviewVO {
+   
+   private Long reviewId;
+   private String email;
+   private Long lectureId;
+   private String regDate;
+   private int starRating;
+   private String contents;
+   private String approval;
 }
