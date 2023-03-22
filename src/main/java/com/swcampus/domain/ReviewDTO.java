d0ec3fe8e8d842e921dcbcab309a1dc0abbe3358
KHR <podo9264@gmail.com>
2023-03-22 15:58:25 +0900

    2023-03-22 KHR: 베스트 강의 8개 목록 가져오기
    - 강의 정보와 함께 가져오기 위해 DTO 멤버변수 추가

--- src/main/java/com/swcampus/domain/ReviewDTO.java
+++ src/main/java/com/swcampus/domain/ReviewDTO.java
@@ -1,10 +1,17 @@
 package com.swcampus.domain;
 
+import java.util.Date;
+
 public class ReviewDTO {
 	
 	private Long lectureId;
 	private String title;
 	private String agency;
-	private 
+	private String target;
+	private String method;
+	private String category;
+	private String teacher;
+	private Date startDate;
+	private Date endDate;
 	private float avg;
 }
